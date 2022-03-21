b499ecd359fdcf72e7e4b5f97aa09fc9aa592d78
ChrisrtophTrischler <73523526+ChrisrtophTrischler@users.noreply.github.com>
2022-03-21 22:08:54 +0100

    display sleep

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,30 @@
 #include <Adafruit_BME280.h>
 #include <lmic.h>
 #include <hal/hal.h>
+#include <FlashStorage.h>
+
 
 #define I2C_OLED_ADDR 0x3C     // 0x3C or 0x3D
 #define I2C_BME280_ADDR 0x76   // 0x76 or 0x77
 
-#define VBATPIN        A7
+#define VBATPIN A7
 
-#define fpin 12
+#define switchpin 12
 #define maxpin 11
 #define minpin 10
+
+#define maxSensorPin A3
+#define minSensorPin A0
+
+#define SensorId (SensorWatching-minSensorPin)
+#define SUmformungMax 75
+#define SUmformungMin 0  
+
+#define DisplayZeit 30
+
+typedef struct{
+  byte data[4];
+}Data;
 
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
@@ -46,13 +61,36 @@ const String hzvorlage= "Frequez: fhz";
 const String prozentvorlage = "Feuchtigkeit: p%";
 const String tempvorlage = "Temperatur: i^C";
 
-int maxWert = 50;
-int minWert = 200;
+int maxWert[4] = {200,200,200,200};
+int minWert[4] = {0,0,0,0};
+int SensorWatching = minSensorPin;
+FlashStorage(maxWert_fs, Data);
+FlashStorage(minWert_fs, Data);
+
+long unsigned int lastsetmin = 0;
+long unsigned int lastsetmax = 0;
+long unsigned int lastPressedSwitch = 0;
+long unsigned int displayTime =0;
+bool lastStatus = false;
+bool powerSaveMode = true;
 
 uint8_t leero[104];
 uint8_t leeru[104];
 uint8_t varo[sizeof(leero)];
 uint8_t varu[sizeof(leero)];
+
+void readData(FlashStorageClass<Data> fs, int *n, int plus){
+  Data x = fs.read();
+  for(int i=0;i<4;i++) 
+    if( x.data[i] != 0 ) n[i] = x.data[i] + plus;
+}
+
+void writeData(FlashStorageClass<Data> fs, int *n, int minus){
+  Data x;
+  for(int i=0;i<4;i++) 
+    x.data[i] = n[i] - minus;
+  fs.write(x);
+}
 
 int frequenz(int pin){
   int start = micros();
@@ -64,7 +102,6 @@ int frequenz(int pin){
       if(wert==1)wechsel++;
       last = wert;
     }
-    delayMicroseconds(1);
   }
   return (int)((wechsel*1000.0)/(micros()-start));
 }
@@ -91,18 +128,23 @@ void displayupdate(){
   vbat /= 1024; // convert to voltage
   int pbat = ((vbat-3.3)/0.9)*100;
   float temperature = bme280.readTemperature();
-  int frequenzfeutigkeit = frequenz(fpin);
-  int prozentfeuchtigkeit = (1-(((double)frequenzfeutigkeit-minWert))/(maxWert-minWert))*100;
+  if(isnan(temperature)){
+    if(!bme280.begin(I2C_BME280_ADDR)) display.drawString(0,0,"BME280 not found");
+    else temperature = bme280.readTemperature();
+  }
+  int frequenzfeutigkeit = frequenz(SensorWatching);
+  int prozentfeuchtigkeit = (1-(((double)frequenzfeutigkeit-minWert[SensorId]))/(maxWert[SensorId]-minWert[SensorId]))*100;
 
   String hz = hzvorlage;
   hz.replace("f",String(frequenzfeutigkeit));
   String ps = String("p%");
   ps.replace("p",String(prozentfeuchtigkeit));
+
   String ts = tempvorlage;
   ts.replace("i",String(temperature));
 
   char datastr[17]; 
-  hz.toCharArray(datastr,6,9);
+  hz.toCharArray(datastr,6,9);  
   display.clearLine(2);
   display.drawString(0,2,datastr);
 
@@ -136,29 +178,38 @@ void do_send(osjob_t* j){
   } 
   else {
     // Prepare upstream data transmission at the next possible time.
-    char floatStr[17];
     float vbat;
     
     float temperature = bme280.readTemperature();
     float pressure = bme280.readPressure()/100.0f;
     float humidity = bme280.readHumidity();
-    int frequenzfeutigkeit = frequenz(fpin);
-    int prozentfeuchtigkeit = (1-(((double)frequenzfeutigkeit-minWert))/(maxWert-minWert))*100;
+    int prozentfeuchtigkeit[4];
 
     vbat = analogRead(VBATPIN); 
     vbat *= 2;    // resistor on board divided by 2, so multiply back
     vbat *= 3.3;  // Multiply by 3.3V, our reference voltage
     vbat /= 1024; // convert to voltage
 
-    String hz = hzvorlage;
-    hz.replace("f",String(frequenzfeutigkeit));
-    String ps = prozentvorlage;
-    ps.replace("p",String(prozentfeuchtigkeit));
+    for (int i = 0;i+minSensorPin<maxSensorPin;i++){
+      int frequenzfeutigkeit = frequenz(i);
+      if(frequenzfeutigkeit!=0){
+        prozentfeuchtigkeit[i] = (1-(((double)frequenzfeutigkeit-minWert[i]))/(maxWert[i]-minWert[i]))*100;
+
+        String hz = hzvorlage; 
+        hz.replace("f",String(frequenzfeutigkeit));
+        String ps = prozentvorlage;
+        ps.replace("p",String(prozentfeuchtigkeit[i]));
+
+        Serial.println(hz);
+        Serial.println(ps);
+      }
+      
+    }
+
+    
     String ts = tempvorlage;
     ts.replace("i",String(temperature));
 
-    Serial.println(hz);
-    Serial.println(ps);
     Serial.println(ts);
     Serial.println("------------------");
     
@@ -168,16 +219,19 @@ void do_send(osjob_t* j){
     unsigned int p = (unsigned int)((pressure + 5) * 10.0);  // p [300..1100]
     unsigned int h = (unsigned int)((humidity + 0.05) * 10.0);
 
-    unsigned char mydata[9];
+    unsigned char mydata[19];
     mydata[0] = batvalue;
     mydata[1] = (h >> 8) & 0xFF;      
     mydata[2] = h & 0xFF; 
     mydata[3] = (t >> 8) & 0xFF;
     mydata[4] = t & 0xFF;
     mydata[5] = (p >> 8) & 0xFF;
-    mydata[6] = p & 0xFF; 
-    mydata[7] = (unsigned int)(prozentfeuchtigkeit) & 0xFF;
-
+    mydata[6] = p & 0xFF;
+    for(int i=0;i+minSensorPin<maxSensorPin;i++){
+      mydata[3*i+7] = (unsigned short)(minWert[i]) & 0xff;
+      mydata[3*i+8] = (unsigned int)(maxWert[i] - 75) & 0xff;
+      mydata[3*i+9] = (unsigned int)(prozentfeuchtigkeit[i]) & 0xFF; 
+    }
     LMIC_setTxData2(5, mydata, sizeof(mydata), 0);
     Serial.println(F("Packet queued"));
   }
@@ -209,7 +263,7 @@ void onEvent (ev_t ev) {
         u4_t netid = 0;
         devaddr_t devaddr = 0;
         u1_t nwkKey[16];
-        u1_t artKey[16];
+        u1_t artKey[16];  
         LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
         Serial.print("netid: ");
         Serial.println(netid, DEC);
@@ -304,9 +358,7 @@ void onEvent (ev_t ev) {
   }
 }
 
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println(F("Starting"));
 
@@ -316,7 +368,7 @@ void setup() {
   display.begin();
   display.setPowerSave(0);
   display.setFont(u8x8_font_chroma48medium8_r);
-  display.draw1x2String(2,3,"Hello World!");
+  display.draw1x2String(2,3,"Giessomat");
   delay(2000);
   display.clearDisplay();
   display.draw1x2String(0,0,"Feuchtigkeit");
@@ -334,34 +386,81 @@ void setup() {
   // Start job (sending automatically starts OTAA too)
   do_send(&sendjob);
   
-  pinMode(fpin,INPUT_PULLUP); 
+  pinMode(switchpin,INPUT_PULLDOWN); 
   pinMode(maxpin,INPUT_PULLDOWN);
   pinMode(minpin,INPUT_PULLDOWN);
+  for (int i=minSensorPin; i<maxSensorPin; i++) pinMode(i,INPUT_PULLDOWN);
 
   memset(leero,B00000001,sizeof(leero));
   memset(leeru,B10000000,sizeof(leeru));
   leero[0]=leeru[0]=leero[103]=leeru[103]=B11111111;
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  //Flashspeicher lesen
+  readData(maxWert_fs, maxWert, SUmformungMax);
+  readData(minWert_fs, minWert, SUmformungMin);
+  
+  displayTime = millis() + 15000;
+}
+
+void activateDisplay(int sec){
+  display.setPowerSave(0);
+  displayupdate();
+  displayTime=millis() + sec * 1000;
+  powerSaveMode = false;
+  Serial.println("display on");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   os_runloop_once();
-  digitalWrite(LED_BUILTIN,LOW);
-  if(digitalRead(maxpin)){ 
-    digitalWrite(LED_BUILTIN,HIGH);  
-    maxWert = frequenz(fpin);
+
+  if(digitalRead(maxpin) && millis() > lastsetmax){//set Frequenzy with most humidity
+    maxWert[SensorId] = frequenz(SensorWatching);
+    writeData(maxWert_fs, maxWert, SUmformungMax);
     Serial.print("Max: ");
-    Serial.println(maxWert);
-    displayupdate();
+    Serial.println(maxWert[SensorId]);
+    activateDisplay(DisplayZeit);
     display.drawString(5,2,"Max");
+    lastsetmax = millis()+1000;
   }
-  if(digitalRead(minpin)){
-    digitalWrite(LED_BUILTIN,HIGH);
-    minWert = frequenz(fpin);
+
+  if(digitalRead(minpin) && millis() > lastsetmin){//set Frequenz mit geringster Feuchtigkeit
+    minWert[SensorId] = frequenz(SensorWatching);
+    writeData(minWert_fs, minWert, SUmformungMin);
     Serial.print("Min: ");
-    Serial.println(minWert);
-    displayupdate();
+    Serial.println(minWert[SensorId]);
+    activateDisplay(DisplayZeit);
     display.drawString(5,2,"Min");
+    lastsetmin = millis()+1000;
   } 
-}
+
+  if(lastStatus){
+    if(millis() > lastPressedSwitch){
+      if(!digitalRead(switchpin)){
+        lastStatus=false;
+        lastPressedSwitch=millis()+100; 
+      }
+    }
+    else if(!digitalRead(switchpin)){
+      if(++SensorWatching>maxSensorPin) SensorWatching=minSensorPin;
+      display.drawString(0,3,"Sensor: ");
+      char buffer[2];
+      String(SensorId).toCharArray(buffer,2,0); 
+      display.drawString(8,3,buffer);
+      lastPressedSwitch=millis()+100;
+      lastStatus=false; 
+    }
+  }
+  else if(digitalRead(switchpin) && millis() > lastPressedSwitch){ 
+    Serial.print("display ...");
+    lastStatus = true;
+    lastPressedSwitch=millis()+500;
+    activateDisplay(DisplayZeit);
+  }
+
+  if(!powerSaveMode && millis() > displayTime){
+    display.setPowerSave(1); //display off
+    powerSaveMode = true;
+    Serial.println("Display off");
+  }
+} 
